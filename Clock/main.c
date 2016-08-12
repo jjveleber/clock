a6ccc53d0b16cb0a97bab713833c1b110c13fa0f
jjveleber <git@jeremyveleber.com>
2016-08-12 07:58:07 -0700

    Initial commit

---
+++ Clock/main.c
@@ -0,0 +1,18 @@
+/*
+ * Clock.c
+ *
+ * Created: 8/12/2016 7:52:25 AM
+ * Author : jjveleber
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
